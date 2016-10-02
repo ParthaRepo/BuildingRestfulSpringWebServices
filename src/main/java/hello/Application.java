90e57f4d1cd7da811ffb8ab54fa8269c675ea263
Logakannan <PXL8108@homedepot.com>
2016-10-02 00:11:17 -0400

    added the project files

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package main.java.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by PXL8108 on 10/2/2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
